eb57ce4f24763770440a3238c86d6d8bf4d99ea2
Swapnil Sonar <39788258+swapnilsonar89@users.noreply.github.com>
2018-07-26 17:10:18 -0500

    Add files via upload

---
+++ Functions - Methods/element count.py
@@ -0,0 +1,16 @@
+# counts the repeating element in an array
+
+def noCount(nums):
+    c={}
+    count=0
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[i] == nums[j]:
+                count+=1
+            elif j == (len(nums)-1):
+                   c[nums[i]]=count
+                   count=0
+    return c
+        
+                
+            
