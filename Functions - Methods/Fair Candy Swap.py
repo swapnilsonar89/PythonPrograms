6b2697cfd9cada0332241dfb5ef87a2a8ae65db4
Swapnil Sonar <39788258+swapnilsonar89@users.noreply.github.com>
2018-10-18 00:29:29 -0500

    Update Fair Candy Swap.py

--- Functions - Methods/Fair Candy Swap.py
+++ Functions - Methods/Fair Candy Swap.py
@@ -1,29 +1,34 @@
-def sol(a,b):
-    out = []
-    swap = []
-    bal = (sum(a) + sum(b))/2
-    print(bal)
-    if sum(a) > bal:
-        for i in a:
-            if i + sum(b) > bal and sum(a) - i < bal:
-                             
-                for j in b:
-                    if j + sum(a) - i == bal:
-                        swap.append(i)
-                        swap.append(j)
+class Solution:
+    def fairCandySwap(self, a, b):
+        swap = []
+        bal = (sum(a) + sum(b))/2
+        f = 0
+        if sum(a) > bal:
+            for i in a:
+                print(i)
+                if i + sum(b) > bal and sum(a) - i < bal:
+                    for j in b:
+                        if j + sum(a) - i == bal:
+                            swap.append(i)
+                            swap.append(j)
+                            f = 1
+                            break
+                    if f == 1:
                         break
-            break
-    elif sum(b) > bal:
-        for i in b:
-            if i + sum(a) > bal and sum(b) - i < bal:
-                for j in a:
-                    if j + sum(b) -i == bal:
-                        swap.append(i)
-                        swap.append(j)
+                
+                        
+        elif sum(b) > bal:
+            for i in b:
+                if i + sum(a) > bal and sum(b) - i < bal:
+                    for j in a:
+                        if j + sum(b) -i == bal:
+                            swap.append(j)
+                            swap.append(i)
+                            f = 1
+                            break
+                    if f == 1:
                         break
-            break
+            
                 
                 
-    return swap
-                
-  
+        return swap
