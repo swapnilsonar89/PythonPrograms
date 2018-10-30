f77b0dc5601a6d2e6bd6a46ff10d3296d51fffcd
Swapnil Sonar <39788258+swapnilsonar89@users.noreply.github.com>
2018-10-30 16:02:55 -0500

    Update prime numbers.py

--- Functions - Methods/prime numbers.py
+++ Functions - Methods/prime numbers.py
@@ -1,21 +1,24 @@
+def is_prime(x):
+    for i in range(2,x):
+        if x%i == 0:
+            return False
+    return True
+    
+    
+
+
+
 def primes(nums):
     prime_list=[]
     if nums<2:
         return 0
     else:
-        count=2
-        prime_list.append(2)
-        prime_list.append(3)
-        for i in range(4,nums+1):
-            x=i
-            a=0
-            for j in range(1,nums+1):
-                if x%j==0:
-                    a+=1
-            if a==2:
-                count+=1
-                prime_list.append(x)
+        count = 0
+        for i in range(2,nums+1):
+            if is_prime(i):
+                prime_list.append(i)
+                count += 1
+               
         print(prime_list)
                 
         return count
-                
